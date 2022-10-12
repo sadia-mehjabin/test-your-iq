54bc8e98b16b1d0573223b1d7c135d1e20e88249
sadia-mehjabin <jhidnicraft@gmail.com>
2022-10-12 20:13:17 +0600

    toast added

--- src/components/quiz-display/Quiz-display.js
+++ src/components/quiz-display/Quiz-display.js
@@ -1,25 +1,31 @@
 import React from 'react';
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const QuizDisplay = ({item}) => {
     const {question, options} = item;
     const input = options.map(option=> option);
+    // const notify = () => 
     const optionHandler = (id)=> {
        if(id === item.correctAnswer){
-        alert('this is right')
+        toast("this is right");
         } 
         else{
-            alert('this is wrong')
+            
         }
+    }
+    const eyeButtonHandler = ()=>{
+        toast(item.correctAnswer)
     }
     
     return (
         <div>
-            <h3>{question}</h3>
+            <h3>{question} <button onClick={eyeButtonHandler}>hello</button></h3>
             <ul>
             <input type="radio" value={input[0]} onClick={()=>optionHandler(input[0])} name="quiz" /> {input[0]} <br />
             <input type="radio" value={input[1]} onClick={()=>optionHandler(input[1])} name="quiz" /> {input[1]} <br />
             <input type="radio" value={input[2]} onClick={()=>optionHandler(input[2])} name="quiz" /> {input[2]} <br />
             <input type="radio" value={input[3]} onClick={()=>optionHandler(input[3])} name="quiz" /> {input[3]} <br />
+            <ToastContainer></ToastContainer>
 
             </ul>
 
